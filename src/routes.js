aa48183c740711c6dfcebde105f6adaac825b71f
akramsheriff5 <akramsheriff05042000@gmail.com>
2025-01-11 13:02:05 +0530

    live

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
-import AdminPanel from "./components/AdminPanel";
 import Signup from "./components/Signup";
 import ClientDashboard from "./components/ClientDashboard";
 import ServiceProviderDashboard from "./components/ServiceProviderDashboard";
+import AdminPanel from "./components/AdminPanel";
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      {/* Login Route */}
+    <Routes basename="/webworrk">
       <Route path="/" element={<Login />} />
-
-      {/* Signup Route */}
       <Route path="/signup" element={<Signup />} />
-
-      {/* Dashboards */}
       <Route path="/ClientDashboard" element={<ClientDashboard />} />
       <Route path="/ServiceProviderDashboard" element={<ServiceProviderDashboard />} />
-      <Route path="/AdminDashboard" element={<AdminPanel />} />
+      <Route path="/AdminPanel" element={<AdminPanel />} />
     </Routes>
   );
 };
